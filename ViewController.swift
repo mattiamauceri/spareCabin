32fb6f078e8e7b6879f30ebbf748b249b66af9e6
Mattia Mauceri <mattia.mauceri@ly.st>
2022-03-18 22:29:32 +0000

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpareCabin
+//
+//  Created by Mattia Mauceri on 18/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
